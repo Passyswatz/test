3185f31e9a8211822a90eb3d5c419ce63048adbc
passyswatz <passyswatz@gmail.com>
2023-06-25 16:28:23 +0100

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+function sum(A,B){
+    console.log(A + B)
+}
+sum(5,4)
+// let A = 5;
+// let B = 6;
+function sayName(A,B){
+   return A * B;
+}
+
+let result = sayName(10,6);{
+console.log(result)
+}
